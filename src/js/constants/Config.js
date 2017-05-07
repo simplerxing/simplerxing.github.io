d0f52d419f75dfaf783ef7d2e05ab983dadbebbb
simplerxing <simplerxing@gmail.com>
2017-05-07 21:32:00 +0800

    test

--- src/js/constants/Config.js
+++ src/js/constants/Config.js
@@ -6,7 +6,7 @@ export const CONFIG = {
   titleLoad  : 'simplerxing - 正在加载...',
   duoshuo    : '6417351428376888000',
   //duoshuo    : 'cobish',
-  baiduAnaly : 'jiandansimple',
+  baiduAnaly : '44271c2a09c65db7418304d9ddd9419a',
   //baiduAnaly : '84602bc3713ca2aeb0258c55adf6a333',
   // access_token : '6117ebab6cc1cffee7f8d82191c8c937714e05ea'
 };
